a13c801d3192d3e99c0147c99320b8c24c47ba8c
Mike Ogar <ogar.m.v@mail.ru>
2023-07-14 15:25:13 +0300

    "Add photo"

--- src/main/resources/static/js/jsForMainPage.js
+++ src/main/resources/static/js/jsForMainPage.js
@@ -13,30 +13,31 @@ $("#load_photo").change(function() {
          img.style.border="2px solid black";
          img.style.objectFit="cover";
 
-         var text_photo = document.getElementById("get_photo");
-         text_photo.value = load_img.src;
+         var photo = {};
+         photo[0] = img;
+         console.log("One element photo '" + photo[0] + ".");
       }
       reader.readAsDataURL(this.files[0]);
    }
 });
 
 //Full update data
-$("#button_save_photo").click(function() {
-   var data = $("#form_form_photo").serialize();
-   console.log("Method add photo. Data: " + data + ".")
-   $.ajax ({
-      method: "POST",
-      url: "/addPhoto/",
-      data: data,
-      success: function(response) {
-         var photo = {};
-         photo.id = response;
-         console.log("Method add photo. Response: " + response + ".");
-         var dataArray = $("#form_form_photo").serializeArray();
-         for (i in dataArray) {
-            photo[dataArray[i]["photo"]] = dataArray[i]["value"];
-         }
-      }
-   });
-   return false;
-});
+//$("#button_save_photo").click(function() {
+//   var data = $("#form_form_photo").serialize();
+//   console.log("Method add photo. Data: " + data + ".")
+//   $.ajax ({
+//      method: "POST",
+//      url: "/addPhoto/",
+//      data: data,
+//      success: function(response) {
+//         var photo = {};
+//         photo.id = response;
+//         console.log("Method add photo. Response: " + response + ".");
+//         var dataArray = $("#form_form_photo").serializeArray();
+//         for (i in dataArray) {
+//            photo[dataArray[i]["photo"]] = dataArray[i]["value"];
+//         }
+//      }
+//   });
+//   return false;
+//});
